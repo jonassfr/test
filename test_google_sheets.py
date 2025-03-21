b3ab5c055923f09a0aea53cc5124db6cb8afd22f
jonassfr <jonas.schaefer1311@outlook.de>
2025-03-20 21:31:36 -0400

    Update test_google_sheets.py

--- test_google_sheets.py
+++ test_google_sheets.py
@@ -4,6 +4,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Lade Google Credentials aus Streamlit Secrets
 try:
+    creds_json = st.secrets["GOOGLE_CREDENTIALS"]
     creds_dict = dict(st.secrets["GOOGLE_CREDENTIALS"])  # Stelle sicher, dass es ein Dictionary ist
     st.write("🔍 Private Key Vorschau:", creds_json["private_key"][:50] + "...")
    
