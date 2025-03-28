902b6c958dbf912379e60e3a30cd1ec8baedc8aa
jonassfr <jonas.schaefer1311@outlook.de>
2025-03-27 20:04:19 -0400

    Update test_google_sheets.py

--- test_google_sheets.py
+++ test_google_sheets.py
@@ -51,7 +51,14 @@ with st.form("new_entry_form"):
     notes = st.text_input("Notes")
     
     is_recurring = st.checkbox("🔁 Recurring Service?")
-    next_service = st.date_input("Next Service Date", disabled=not is_recurring)
+    next_service = ""
+    mileage_last = ""
+    mileage_interval = ""
+
+    if is_recurring:
+        next_service = st.date_input("Next Service Date")
+        mileage_last = st.number_input("Mileage at last service (mi)", min_value=0)
+        mileage_interval = st.number_input("Mileage interval until next service (mi)", min_value=0)is_recurring)
 
     submitted = st.form_submit_button("✅ Save Entry")
     if submitted:
